e9cf00f7c647827de981ca568f7e9040ebbd7a3b
Riri022 <rgoto01@mylangara.ca>
2025-10-14 16:28:16 -0700

    changing database

--- Item-list/addData.js
+++ Item-list/addData.js
@@ -2,35 +2,35 @@ import { firestore } from './firebase-config.js';
 import { addDoc, collection, serverTimestamp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
 
 // storage data
-const clothesData = [
-  {
-    itemName: "Classic White Shirt",
-    category: "Tops",
-    color: "White",
-    size: ["S", "M", "L"],
-    material: "Cotton",
-    season: "Spring",
-    style: ["Casual", "Formal"]
-  },
-  {
-    itemName: "Vintage Denim Jacket",
-    category: "Jackets",
-    color: "Blue",
-    size: ["M", "L"],
-    material: "Denim",
-    season: "Autumn",
-    style: ["Street", "Casual"]
-  },
-  {
-    itemName: "Black Suit Set",
-    category: "Suits",
-    color: "Black",
-    size: ["M", "L"],
-    material: "Wool",
-    season: "Winter",
-    style: ["Formal", "Business"]
-  },
-];
+// const clothesData = [
+//   {
+//     itemName: "Classic White Shirt",
+//     category: "Tops",
+//     color: "White",
+//     size: ["S", "M", "L"],
+//     material: "Cotton",
+//     season: "Spring",
+//     style: ["Casual", "Formal"]
+//   },
+//   {
+//     itemName: "Vintage Denim Jacket",
+//     category: "Jackets",
+//     color: "Blue",
+//     size: ["M", "L"],
+//     material: "Denim",
+//     season: "Autumn",
+//     style: ["Street", "Casual"]
+//   },
+//   {
+//     itemName: "Black Suit Set",
+//     category: "Suits",
+//     color: "Black",
+//     size: ["M", "L"],
+//     material: "Wool",
+//     season: "Winter",
+//     style: ["Formal", "Business"]
+//   },
+// ];
 
 async function addAllClothes() {
   try {
